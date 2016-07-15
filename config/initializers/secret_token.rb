2a3418017000950e8f5bf9384e198d2770ed02df
Rolando Alvarado <rorroland@gmail.com>
2016-07-16 06:07:31 +0800

    Added Initial Files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlShortener::Application.config.secret_key_base = '8702837c36696a260bd5c0fd585923df9e2c8c8b302e6408680552802b2659dcbaf7cbde45ec74d11d802871c8b2592790e77619f5e76fcffd46de70e9329394'
